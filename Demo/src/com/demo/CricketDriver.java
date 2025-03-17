e45777506372a5b2d96880eee903bde8aeeb5e2a
Dell <Dell@DESKTOP-CK3LNQH>
2025-03-17 17:43:56 +0530

    Add

---
+++ Demo/src/com/demo/CricketDriver.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import demo.Cricket;
+
+public class CricketDriver {
+
+	public static void main (String[]args)
+	{
+		Cricket  c1 = new Cricket("CSK",7);
+		c1.yellow();
+	}
+public void yellow()
+{
+System.out.println(name + a);
+
+}
---
+++ Demo/src/com/demo/Demo.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class Demo {
+	public static void main(String[] args) {
+		String name ="my name is JAY";
+		int age=21;
+		char gender='M';
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(gender);
+	}
+
+}
